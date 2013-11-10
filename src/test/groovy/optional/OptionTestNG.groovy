7ab92df249ca9d0a326314644db990576f3e3778
devpie <mjan@dfi-net.de>
2013-11-10 15:17:58 +0100

    Trying to test with TestNG... Not working yet... skips the test...

---
+++ src/test/groovy/optional/OptionTestNG.groovy
@@ -0,0 +1,11 @@
+package optional
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: jan
+ * Date: 11/10/13
+ * Time: 11:14 AM
+ * To change this template use File | Settings | File Templates.
+ */
+class OptionTestNG {
+}
